ffd52c74738d2b5f7a27f01f55fb98bf2d857c84
DannielVo <phattinhoc2017@gmail.com>
2026-05-03 10:41:28 +0700

    Feat: Setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "../../../Final/flashup/client/src/pages/auth/Login";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/home/Home";
+import Products from "./pages/home/Products";
+import ProductDetail from "./pages/home/ProductDetail";
+import NotFound from "../../../Final/flashup/client/src/pages/404/NotFound";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetail />} />
+        {/* <Route path="admin" element={<Admin />} />
+      <Route path="admin/products" element={<AdminProducts />} /> */}
+      </Route>
+
+      {/* 404 */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
